943e3387f23bb856437ff4590d5f1f24dfacab6a
Apiux <apiux@apiuxnb1391s-MacBook-Pro.local>
2021-09-02 13:55:23 -0400

    firt commit automation

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@RunWith(SerenityRunner.class)
+public class LoginTest extends PageObject {
+
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+    @FindBy(name="q")
+    WebElement googlesearch;
+
+
+    @Title("Default Web URL loading from webdriver.base.url set in serenity.properties file.")
+    @Test
+    public void doLogin(){
+
+        open();
+        //driver.get("http://google.com");
+        //driver.findElement(By.name("q")).sendKeys("Hello Sereniti!!!");
+        //$(By.name("q")).sendKeys("Hello Sereniti!!!");
+        typeInto(googlesearch, "Hello Sereniti!!!");
+    }
+
+
+}
